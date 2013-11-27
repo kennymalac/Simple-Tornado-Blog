0ee93faf775886080a1d12d991382c80fea9aa9f
Kenneth Malac <kennethm777@gmail.com>
2013-11-27 13:48:21 -0800

    Added docstrings and Page base class.

--- src/main.py
+++ src/main.py
@@ -12,13 +12,22 @@ from tornado.options import define, options
 define("port", default=8888, help="run server on given port", type=int)
 
 class Application(tornado.web.Application):
+    """Base tornado web class for our application."""
     def __init__(self):
         handlers = []
         settings = dict()
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
+class Page(RequestHandler):
+    """Base class for pages."""
+    def fail(self, message):
+        """Send error message to be displayed."""
+        self.write(message)
+
+
 def main():
+    """Start our web application."""
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(options.port)
