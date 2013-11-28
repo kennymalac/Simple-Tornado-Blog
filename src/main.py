e1a345389d068dbc481c5174b99f8ddf10cc3524
Kenneth Malac <kennethm777@gmail.com>
2013-11-28 11:23:16 -0800

    Added mongodb interfacing and postlist.

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ define("port", default=8888, help="run server on given port", type=int)
 
 current_dir = os.path.dirname(__file__)
 client = pymongo.MongoClient()
-db = client['tornado-blog']
+db = client["tornado-blog"]
 
 
 class Page(tornado.web.RequestHandler):
@@ -27,6 +27,20 @@ class Page(tornado.web.RequestHandler):
 class IndexPage(Page):
     def get(self):
         self.render("index.html")
+
+
+class PostsPage(Page):
+    def compile_posts(self, page_num):
+        pagination = 10
+        db.posts.ensure_index("timestamp", -1)
+        return db.posts.find().sort("timestamp", -1).skip(page_num * pagination).limit(pagination)
+
+    def get(self, page_num=1):
+        index = page_num - 1
+        self.render(
+                "posts.html",
+                posts=self.compile_posts(index)
+        )
 
 
 def main():
@@ -42,6 +56,7 @@ def main():
 
     handlers = [
         (r"/", IndexPage),
+        (r"/posts", PostsPage),
     ]
 
     application = tornado.web.Application(handlers, **settings)
